c963c69857eeba261abc2ed54fb30217b02c1c1e
hariskresna <haris.ixd18@gmail.com>
2018-07-10 20:39:36 +0700

    push pertama

---
+++ application/controllers/Admin.php
@@ -0,0 +1,88 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('admin/dashboard');
+	}
+
+	public function login()
+	{
+		$this->load->view('admin/login');
+	}
+
+	public function register()
+	{
+		$this->load->view('admin/register');
+	}
+
+	public function buku()
+	{
+		$this->load->model('M_admin');
+		$data = $this->M_admin->GetData('buku');
+		$data = array('data' => $data);
+		$this->load->view('admin/buku', $data);
+	}
+
+	public function tambahbuku()
+	{
+		$this->load->view('admin/tambahbuku');
+	}
+
+	public function prosestambahbuku()
+	{
+		$this->load->model('M_admin');
+    	$data = array(
+	        'kode' => $this->input->post('kode'),
+	        'judul' => $this->input->post('judul'),
+	        'lokasi' => $this->input->post('lokasi')
+        );
+	    $data = $this->M_admin->Insert('buku', $data);
+	    redirect('admin/tambahbuku','refresh');
+	}
+
+	public function editbuku($kode)
+	{
+		$this->load->model('M_admin');
+	    $buku = $this->M_admin->GetWhere('buku', array('kode' => $kode));
+	    $data = array(
+	        'kode' => $buku[0]['kode'],
+	        'judul' => $buku[0]['judul'],
+	        'lokasi' => $buku[0]['lokasi'],
+        );
+		$this->load->view('admin/editbuku', $data);
+	}
+
+	public function proseseditbuku()
+	{
+		$kode = $_POST['kode'];
+		$judul = $_POST['judul'];
+		$lokasi = $_POST['lokasi'];
+
+		$data = array(
+			'judul' => $judul,
+			'lokasi' => $lokasi
+		);
+
+		$where = array(
+			'kode' => $kode,
+		);
+
+		$this->load->model('M_admin');
+		$query = $this->M_admin->Update('buku', $data, $where);
+		if( $query > 0 )
+		{
+			redirect('admin/buku', 'refresh');
+		}
+	}
+
+	public function deletebuku($id)
+	{
+		$id = array('id' => $id);
+		$this->load->model('M_admin');
+		$this->M_admin->Delete('buku', $id);
+		redirect('admin/buku', 'refresh');
+	}
+}
---
+++ application/views/admin/src/sidebar.php
@@ -0,0 +1,36 @@
+<!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <!--<p>Alexander Pierce</p>-->
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MAIN NAVIGATION</li>
+        <li class="active">
+          <a href="<?php echo base_url('index.php/admin'); ?>">
+            <i class="fa fa-dashboard"></i> <span>INFO PERPUSTAKAAN</span>
+          </a>
+        </li>
+        <li class="">
+          <a href="<?php echo base_url('index.php/admin/buku'); ?>">
+            <i class="fa fa-files-o"></i>
+            <span>DAFTAR BUKU</span>
+          </a>
+        </li>
+        <li class="">
+          <a href="<?php echo base_url('index.php/admin/tambahbuku'); ?>">
+            <i class="fa fa-files-o"></i>
+            <span>TAMBAH BUKU</span>
+          </a>
+        </li>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
